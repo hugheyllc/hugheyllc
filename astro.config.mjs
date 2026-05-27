518e841f3de613f4b242eb6e7bb91bef0e91cc5b
Joe Hughey <joe@joehughey.com>
2026-05-27 16:38:39 -0400

    fix: revert broken rewrites, add trailingSlash for Vercel static routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ export default defineConfig({
   site: 'https://hugheyllc.com',
   integrations: [sitemap()],
   output: 'static',
+  trailingSlash: 'always',
   adapter: vercel({
     webAnalytics: {
       enabled: true,
